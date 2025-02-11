886d9ae3a55c6b06b0e81c4810ec9b25fa1996f9
Quentin RA <quentin.ramsamy.cf@gmail.com>
2025-02-11 14:35:16 +0100

    [feat] version 1.0.2

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -14,7 +14,7 @@ var banner = (`
  ########  ###        ###       ###     ### 
 `)
 
-var version = "v1.0.0"
+var version = "v1.0.2"
 
 // showBanner is used to show the banner to the user
 func showBanner() {
